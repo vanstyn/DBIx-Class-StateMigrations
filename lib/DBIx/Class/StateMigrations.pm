6b77e97ae689a397ecbd0b9994d273b180cc64a2
Henry Van Styn <vanstyn@intellitree.com>
2020-11-23 20:29:00 -0500

    WIP progress on main class + SchemaState class

--- lib/DBIx/Class/StateMigrations.pm
+++ lib/DBIx/Class/StateMigrations.pm
@@ -1,26 +1,100 @@
-package DBIx::Class::BlockchainMigrations;
+package DBIx::Class::StateMigrations;
 
 use strict;
 use warnings;
 
 # ABSTRACT: Schema migrations via checksums instead of versions
 
-our VERSION = '0.01';
+our $VERSION = '0.01';
 
 use Moo;
 use Types::Standard qw(:all);
+use Scalar::Util 'blessed';
+use Try::Tiny;
 
 use Path::Class qw/file dir/;
 use DBIx::Class::Schema::Diff;
+use DBIx::Class::Schema::Diff::State;
+use DBIx::Class::StateMigrations::SchemaState;
+#use DBIx::Class::StateMigrations::Migration;
+
+has 'migrations_dir', is => 'ro', default => sub { undef };
+has 'connected_schema', is => 'ro', required => 1, isa => InstanceOf['DBIx::Class::Schema'];
+
+has 'schema_class', is => 'ro', lazy => 1, init_arg => 1, default => sub {
+  my $self = shift;
+  blessed $self->connected_schema ? blessed $self->connected_schema : $self->connected_schema
+};
+
+has 'loader_options', is => 'ro', default => sub {{
+  naming => { ALL => 'v7'},
+  use_namespaces => 1,
+  use_moose => 0,
+  debug => 0,
+  qualify_objects => 1
+}}, isa => HashRef;
 
 
-has 'schema', is => 'ro', isa => InstanceOf['DBIx::Class::Schema'];
+has 'diff_filters', is => 'ro', default => sub {[
+  filter_out => 'isa'
+]}, isa => ArrayRef;
 
 
-has 'Migrations'
+has 'Migrations', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  if(my $dir = $self->migrations_dir) {
+  
+  
+  }
+  else {
+    return []
+  }
+}, isa => ArrayRef[InstanceOf['DBIx::Class::StateMigrations::Migration']];
+
+
+has 'connect_info_args', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  $self->connected_schema->storage->connect_info
+}, isa => Ref;
+
+has 'loaded_schema_class', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  
+  my $schema_class = $self->schema_class || 'ScannedSchmeaForMigration';
+  
+  my $ref_class = join('_',$schema_class,'RefSchema',String::Random->new->randregex('[a-z0-9A-Z]{5}'));
+  
+  DBIx::Class::Schema::Loader::make_schema_at(
+    $ref_class => $self->loader_options, $self->connect_info_args  
+  ) or die "Loading schema failed";
+
+  $ref_class
+}, isa => Str;
 
 
 
+has 'current_SchemaState', is => 'ro', lazy => 1, default => sub {
+  my $self = shift;
+  
+  my $State = DBIx::Class::Schema::Diff->state(
+    schema => $self->loaded_schema_class
+  );
+  
+  DBIx::Class::StateMigrations::SchemaState->new(
+    DiffState    => $State,
+    diff_filters => $self->diff_filters
+  )
+}, isa => InstanceOf['DBIx::Class::StateMigrations::SchemaState'];
+
+
+
+
+sub BUILD {
+  my $self = shift;
+  try{$self->connected_schema->storage->connected} or die join('',
+    'Supplied connected_schema "', $self->connected_schema, '" is not connected'
+  )
+}
 
 
 1;
@@ -29,11 +103,11 @@ __END__
 
 =head1 NAME
 
-DBIx::Class::Schema::BlockchainMigrations - Schema migrations via checksums instead of versions
+DBIx::Class::Schema::StateMigrations - Schema migrations via checksums instead of versions
 
 =head1 SYNOPSIS
 
- use DBIx::Class::Schema::BlockchainMigrations;
+ use DBIx::Class::Schema::StateMigrations;
  
  ...
  
